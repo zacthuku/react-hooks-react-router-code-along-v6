188d566d46e52784b5c35133c13558cbab47e007
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-20 21:03:55 +0300

    routes lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.js";
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/Login.js
@@ -0,0 +1,30 @@
+import NavBar from "../components/NavBar";
+
+
+function Login() {
+    return (
+      <>
+        <header>
+        <NavBar />
+        </header>
+        <main>
+          <h1>Login</h1>
+          <form>
+            <div>
+              <label for="username">Username: </label>
+              <input id="username" type="text" name="username" placeholder="Username" />
+            </div>
+            <br/>
+            <div>
+              <label for="password">Password: </label>
+              <input id="password" type="password" name="password" placeholder="Password" />
+            </div>
+            <br/>
+            <button type="submit">Submit</button>
+          </form>
+        </main>
+      </>
+    );
+  };
+  
+  export default Login;
